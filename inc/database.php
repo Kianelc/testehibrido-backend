0d19208c089213b9c094eea257d99b86cee57067
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2020-07-12 22:32:43 -0300

    Adição das funções de edição, remoção e salvar

--- inc/database.php
+++ inc/database.php
@@ -49,3 +49,64 @@ function find( $table = null, $id = null ) {
 function find_all($table) {
 	return find($table);
 }
+
+function save($table = null, $data = null) {
+	$database = open_database();
+	$columns = null;
+	$values = null;
+	foreach ($data as $key => $value) {
+	  $columns .= trim($key, "'") . ",";
+	  $values .= "'$value',";
+	}
+	$columns = rtrim($columns, ',');
+	$values = rtrim($values, ',');
+	$sql = "INSERT INTO " . $table . "($columns)" . " VALUES " . "($values);";
+	try {
+	  $database->query($sql);
+	  $_SESSION['message'] = 'Registro cadastrado com sucesso.';
+	  $_SESSION['type'] = 'success';
+	} catch (Exception $e) { 
+	  $_SESSION['message'] = 'Nao foi possivel realizar a operacao.';
+	  $_SESSION['type'] = 'danger';
+	} 
+	close_database($database);
+  }
+
+  function update($table = null, $id = 0, $data = null) {
+	$database = open_database();
+	$items = null;
+	foreach ($data as $key => $value) {
+	  $items .= trim($key, "'") . "='$value',";
+	}
+	$items = rtrim($items, ',');
+	$sql  = "UPDATE " . $table;
+	$sql .= " SET $items";
+	$sql .= " WHERE id=" . $id . ";";
+	try {
+	  $database->query($sql);
+	  $_SESSION['message'] = 'Registro atualizado com sucesso.';
+	  $_SESSION['type'] = 'success';
+	} catch (Exception $e) { 
+	  $_SESSION['message'] = 'Nao foi possivel realizar a operacao.';
+	  $_SESSION['type'] = 'danger';
+	} 
+	close_database($database);
+  }
+
+function remove( $table = null, $id = null ) {
+	$database = open_database();
+	try {
+	  if ($id) {
+		$sql = "DELETE FROM " . $table . " WHERE id = " . $id;
+		$result = $database->query($sql);
+		if ($result = $database->query($sql)) {   	
+		  $_SESSION['message'] = "Registro Removido com Sucesso.";
+		  $_SESSION['type'] = 'success';
+		}
+	  }
+	} catch (Exception $e) { 
+	  $_SESSION['message'] = $e->GetMessage();
+	  $_SESSION['type'] = 'danger';
+	}
+	close_database($database);
+}
