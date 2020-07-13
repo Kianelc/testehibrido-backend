c7d48794a3e5ceac15d8e9358ec432a39ecdbd0c
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2020-07-12 22:31:28 -0300

    Ajuste na estilização da página

--- customers/add.php
+++ customers/add.php
@@ -5,37 +5,35 @@
 
 <?php include(HEADER_TEMPLATE); ?>
 
-<h2>Novo Cliente</h2>
-
-<form action="add.php" method="post">
-  <!-- area de campos do form -->
-    <hr />
-    <div class="row">
-        <div class="form-group col-md-7">
-            <label for="name">Nome Completo</label>
-            <input type="text" class="form-control" name="customer['name']">
+<div class="container mt-5 px-5">
+    <h2>Novo Cliente</h2>
+    <form action="add.php" method="post" class="mt-5">
+        <div class="row">
+            <div class="form-group col">
+                <label for="name">Nome Completo</label>
+                <input type="text" class="form-control" name="customer['name']">
+            </div>
+            <div class="form-group col">
+                <label for="campo2">CPF</label>
+                <input type="text" class="form-control" name="customer['cpf']">
+            </div>
         </div>
-        <div class="form-group col-md-3">
-            <label for="campo2">CPF</label>
-            <input type="text" class="form-control" name="customer['cpf']">
+        <div class="row">
+            <div class="form-group col">
+                <label for="campo1">Email</label>
+                <input type="text" class="form-control" name="customer['email']">
+            </div>
+            <div class="form-group col">
+                <label for="campo2">Telefone</label>
+                <input type="text" class="form-control" name="customer['phone']">
+            </div>
+        </div>  
+        <div id="actions" class="row">
+            <div class="col text-right mt-3">
+                <button type="submit" class="btn btn-primary">Salvar</button>
+                <a href="index.php" class="btn btn-default">Cancelar</a>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="form-group col-md-5">
-            <label for="campo1">Email</label>
-            <input type="text" class="form-control" name="customer['email']">
-        </div>
-        <div class="form-group col-md-2">
-            <label for="campo2">Telefone</label>
-            <input type="text" class="form-control" name="customer['phone']">
-        </div>
-    </div>  
-    <div id="actions" class="row">
-        <div class="col-md-12">
-            <button type="submit" class="btn btn-primary">Salvar</button>
-            <a href="index.php" class="btn btn-default">Cancelar</a>
-        </div>
-    </div>
-</form>
-
+    </form>
+</div>
 <?php include(FOOTER_TEMPLATE); ?>
